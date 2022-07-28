d61c1637dfb555ae3e313a35754e3b5db143609b
bernardobb <bernardobb.eletronica@gmail.com>
2022-07-28 14:39:40 -0300

    teste list kotlin

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -2,12 +2,14 @@ package one.digitalinnovation.collections
 
 fun main() {
     val joao = Funcionario("Joao",3000.0)
-    val bernardo = Funcionario("Bernardo",4000.0)
+    val bernardo = Funcionario("Bernardo",24000.0)
     val carol = Funcionario("Carol",15000.0)
     val funcionarios = listOf(joao,bernardo,carol)
     funcionarios.forEach{println(it)}
     println(".....")
     println(funcionarios.find { it.nome =="Carol" })
+    println(".....")
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
 }
 
 data class Funcionario(
