53cae29b0312a93fe1e2e0cd663237bae45fecbe
bernardobb <bernardobb.eletronica@gmail.com>
2022-07-28 13:35:46 -0300

    teste arrray kotlin

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteArray.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteArray.kt
@@ -1,2 +1,27 @@
 package one.digitalinnovation.collections
 
+fun main() {
+    val values = IntArray(5)
+    values[0]=0
+    values[1]=6
+    values[2]=4
+    values[3]=5
+    values[4]=8
+
+    for(valor in values){
+        print(valor)
+    }
+    println("  ")
+    values.forEach {
+        print(it)
+    }
+    println("  ")
+    for (index in values.indices){
+        print(values[index])
+    }
+    println("  ")
+    values.sort()
+    for(valor in values){
+        print(valor)
+    }
+}
